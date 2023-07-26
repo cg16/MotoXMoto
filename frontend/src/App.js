f19a5e99ff4525b8880f3294a0285b55d58eb16e
Palafita <ppomoura@gmail.com>
2023-07-26 13:02:19 -0300

    Adicionando algumas partes do backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+
+import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
+import Footer from "./components/Footer/Footer";
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header title="My header" subtitle="subtitle2" />
+      <Main message="My content" />
+      <Footer note="Footer Note" />
     </div>
   );
 }
-
-export default App;
